3f7517a861f54f8333be9b20dfc7be5cc72a7113
Sergiogonzalezpi <bysergy.uny@gmail.com>
2022-05-12 12:38:46 +0200

    Backend Section Page - example to receive message

--- app/src/main/java/com/example/trabbelapp/SectionPage.java
+++ app/src/main/java/com/example/trabbelapp/SectionPage.java
@@ -3,12 +3,23 @@ package com.example.trabbelapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.trabbelapp.models.Activities.Activities;
+import com.example.trabbelapp.models.Activities.Datum;
+import com.example.trabbelapp.utils.ViewTools;
 
 public class SectionPage extends AppCompatActivity {
+
+    private static final String TAG = "Section Page";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_section_page);
+        ViewTools viewTools = new ViewTools();
+        Datum activityCity;
+        activityCity = (Datum) viewTools.reciveSerializableMessageFromIntent(this, "activity");
+        Log.e(TAG, activityCity.getName());
     }
 }

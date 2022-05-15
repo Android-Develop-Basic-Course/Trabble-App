a61657b1be29ae1c7e6916ce637ec1981bda7c65
Sergiogonzalezpi <bysergy.uny@gmail.com>
2022-05-15 20:06:14 +0200

    UX - Logging Activity

--- app/src/main/java/com/example/trabbelapp/LoggingActivity.java
+++ app/src/main/java/com/example/trabbelapp/LoggingActivity.java
@@ -1,8 +1,11 @@
 package com.example.trabbelapp;
 
 import android.os.Bundle;
+import android.text.method.PasswordTransformationMethod;
 import android.util.Log;
+import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +17,8 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class LoggingActivity extends AppCompatActivity {
 
+    private static final int TYPE_TEXT_VARIATION_VISIBLE_PASSWORD = 144;
+    private final int TYPE_TEXT_VARIATION_PASSWORD = 148;
     private final String TAG = "LoggingActivity";
     FirebaseClient firebaseClient;
     EditText emailUser;
@@ -22,6 +27,7 @@ public class LoggingActivity extends AppCompatActivity {
     String email;
     String password;
     ViewTools viewTools;
+    Button visibilityPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +42,7 @@ public class LoggingActivity extends AppCompatActivity {
         emailUser = null;
         passwordUser = null;
 
+
         // Comprobar que no hay usuario registrado
         email = preferenceShareTools.getString("emailUser");
         password = preferenceShareTools.getString("passwordUser");
@@ -45,11 +52,38 @@ public class LoggingActivity extends AppCompatActivity {
             Log.w(TAG, "sharepreference: no");
             setContentView(R.layout.activity_logging);
             findViewById(R.id.loggingLogInButton).setOnClickListener(view -> logIn());
+            findViewById(R.id.loggingPasswordVisibilityButton).setOnClickListener(view -> setVisibilityPassword());
         } else {
             Log.w(TAG, "sharepreference: yes");
             viewTools.changeView(this, HomeActivity.class);
         }
 
+    }
+
+    private void errorLogging() {
+        emailUser.setHint("Re-enter your email");
+        emailUser.setText("");
+        passwordUser.setHint("Re-enter your password");
+        passwordUser.setText("");
+    }
+
+    public void setVisibilityPassword(){
+        emailUser = findViewById(R.id.loggingEmailInput);
+        passwordUser = findViewById(R.id.loggingPasswordInput);
+        visibilityPassword = findViewById(R.id.loggingPasswordVisibilityButton);
+        Log.e(TAG, "visibility -> " + passwordUser.getInputType());
+        if (visibilityPassword.getContentDescription().equals("hide")){
+            visibilityPassword.setContentDescription("show");
+            visibilityPassword.setText(getResources()
+                    .getString(R.string.icon_visibility_visible));
+            passwordUser.setTransformationMethod(null);
+        }
+        else{
+            visibilityPassword.setContentDescription("hide");
+            visibilityPassword.setText(getResources()
+                    .getString(R.string.icon_visibility_invisible));
+            passwordUser.setTransformationMethod(new PasswordTransformationMethod());
+        }
     }
 
 
@@ -60,12 +94,8 @@ public class LoggingActivity extends AppCompatActivity {
         email = emailUser.getText().toString();
         password = passwordUser.getText().toString();
 
-        firebaseClient.loggingFirebase(email, password);
+        firebaseClient.loggingFirebase(this, email, password);
 
-        emailUser.setHint("Re-enter your email");
-        emailUser.setText("");
-        passwordUser.setHint("Re-enter your password");
-        passwordUser.setText("");
         viewTools.hideSystemUI(getWindow().getDecorView());
     }
 
@@ -75,7 +105,7 @@ public class LoggingActivity extends AppCompatActivity {
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = firebaseClient.getmAuth().getCurrentUser();
         if (currentUser != null) {
-            Log.w(TAG, "currentUser: logged");
+            Log.w(TAG, "currentUser: logged -> " + currentUser.getEmail());
         }
     }
 
